ae513a4eb9852a15d36bb8b3ab244182d7fa7ea9
mostafanabil71 <mostafa.nabile.71@gmail.com>
2024-08-02 01:53:28 +0300

    Updating POM File

---
+++ src/main/java/Greating_Webapp/is/a/simple/webapp/which/will/be/used/in/CICD/Labs/GreatingJavaWebappApplication.java
@@ -0,0 +1,13 @@
+package Greating_Webapp.is.a.simple.webapp.which.will.be.used.in.CICD.Labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatingJavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatingJavaWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Greating_Webapp/is/a/simple/webapp/which/will/be/used/in/CICD/Labs/GreatingJavaWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package Greating_Webapp.is.a.simple.webapp.which.will.be.used.in.CICD.Labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatingJavaWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
